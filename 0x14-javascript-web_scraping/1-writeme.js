a24e6a3d6d3220e4f805bb571f4b0eb3d35307d3
kanja23 <martinkaranja92@gmail.com>
2023-07-25 08:27:32 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
 
-function writeStringToFile(filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Successfully wrote to ${filePath}`);
-    }
-  });
-}
-
-if (process.argv.length < 4) {
-  console.error('Usage: node 1-writeme.js <file_path> <content>');
-} else {
-  const filePath = process.argv[2];
-  const content = process.argv[3];
-  writeStringToFile(filePath, content);
-}
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
+});
