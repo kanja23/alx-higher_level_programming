5a943db45c475aebbc92014593e55363710e1b7f
kanja23 <martinkaranja92@gmail.com>
2023-08-01 22:36:47 -0700

    1-main.html

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+// 1-script.js
+
+// Wait for the DOM to be fully loaded before accessing elements
+$(document).ready(function () {
+  // Use the JQuery API to select the <header> element and update its text color to red (#FF0000)
+  $("header").css("color", "#FF0000");
+});
