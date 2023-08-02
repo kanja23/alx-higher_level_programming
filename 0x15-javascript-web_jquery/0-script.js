f8b929ed39c5db5489d79b28d710e6162d3fb9e9
kanja23 <martinkaranja92@gmail.com>
2023-08-01 22:32:43 -0700

    0-main.html

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,10 @@
+// 0-script.js
+
+// Wait for the DOM to be fully loaded before accessing elements
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the <header> element using document.querySelector
+  const headerElement = document.querySelector("header");
+
+  // Update the text color of the <header> element to red (#FF0000)
+  headerElement.style.color = "#FF0000";
+});
