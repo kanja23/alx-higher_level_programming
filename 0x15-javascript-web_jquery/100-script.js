1851726292855c6af4e4bd22a14524d90bdfe28f
kanja23 <martinkaranja92@gmail.com>
2023-08-02 22:26:07 -0700

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// Wait for the DOM to be fully loaded before executing the script
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the header element
+  const headerElement = document.querySelector('header');
+
+  // Update the text color of the header element to red (#FF0000)
+  headerElement.style.color = '#FF0000';
+});
